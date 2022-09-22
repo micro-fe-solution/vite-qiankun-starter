be4add839f656e120cf07cd07a641c21a8e1fd12
wangxingkang <wang_xingkang@qq.com>
2022-09-22 11:14:37 +0800

    chore: update deps

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -6,31 +6,31 @@ const mainAppName = 'launcher';
 const rootDir = join(__dirname, '../examples/');
 
 function getSubApps(appsList: string[]) {
-  return appsList.filter((item) => item.includes('sub'));
+    return appsList.filter((item) => item.includes('sub'));
 }
 
 async function dev(appName: string) {
-  step(`[${appName}] Starting`);
-  const runArgs = ['run', 'dev'];
-  await runCommand('pnpm', runArgs, join(rootDir, appName));
+    step(`[${appName}] Starting`);
+    const runArgs = ['run', 'dev'];
+    await runCommand('pnpm', runArgs, join(rootDir, appName));
 }
 
 async function main() {
-  const apps = readdirSync(rootDir);
-  const { app } = args;
-  if (args.app) {
-    if (apps.includes(app)) {
-      await dev(args.app);
+    const apps = readdirSync(rootDir);
+    const { app } = args;
+    if (args.app) {
+        if (apps.includes(app)) {
+            await dev(args.app);
+        } else {
+            // app not exist
+        }
     } else {
-      // app not exist
+        const subApps = getSubApps(apps);
+        subApps.forEach(async (item) => {
+            await dev(item);
+        });
+        await dev(mainAppName);
     }
-  } else {
-    const subApps = getSubApps(apps);
-    subApps.forEach(async (item) => {
-      await dev(item);
-    });
-    await dev(mainAppName);
-  }
 }
 
 main();
