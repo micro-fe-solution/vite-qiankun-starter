bb2856a5ed7ce19ce0c049e78e1ac8468675e0da
wangxingkang <wang_xingkang@qq.com>
2022-10-24 11:13:51 +0800

    chore: 删除无用文件

--- modules/launcher/src/main.tsx
+++ modules/launcher/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 import '@sensoro-design/styles/dist/styles.min.css';
-import './global.less';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
