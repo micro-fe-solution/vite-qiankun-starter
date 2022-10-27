bbfaf369568d53400eed89656ee406a3b7ab9129
wangxingkang <wang_xingkang@qq.com>
2022-10-28 00:07:49 +0800

    feat: replace logo

--- modules/launcher/src/layouts/BasicLayout.tsx
+++ modules/launcher/src/layouts/BasicLayout.tsx
@@ -2,9 +2,13 @@ import { Outlet } from 'react-router-dom'
 import { GithubFilled, } from '@ant-design/icons';
 import { ProLayout } from '@ant-design/pro-layout';
 
+import logo from '@/assets/logo.svg';
+
 export default function Layout() {
   return (
     <ProLayout
+      title="Vite Qiankun Starter"
+      logo={logo}
       avatarProps={{
         src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
         size: 'small',
