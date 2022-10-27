17cd0478bc8430cabee5990ce99caab66c5e6b97
wangxingkang <wang_xingkang@qq.com>
2022-10-28 00:00:11 +0800

    feat: used @ant-design/pro-layout

--- modules/launcher/src/layouts/BasicLayout.tsx
+++ modules/launcher/src/layouts/BasicLayout.tsx
@@ -1,7 +1,32 @@
-import { BasicLayout } from '@pansy/micro-shared'
+import { Outlet } from 'react-router-dom'
+import { GithubFilled, } from '@ant-design/icons';
+import { ProLayout } from '@ant-design/pro-layout';
 
 export default function Layout() {
   return (
-    <BasicLayout />
+    <ProLayout
+      avatarProps={{
+        src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+        size: 'small',
+        title: (
+          <div
+            style={{
+              color: '#dfdfdf',
+            }}
+          >
+            七妮妮
+          </div>
+        ),
+      }}
+      actionsRender={(props) => {
+        if (props.isMobile) return [];
+
+        return [
+          <GithubFilled key="GithubFilled" onClick={() => { window.open('https://github.com/micro-fe-solution/vite-qiankun-starter') }} />,
+        ]
+      }}
+    >
+      <Outlet />
+    </ProLayout>
   )
 }
