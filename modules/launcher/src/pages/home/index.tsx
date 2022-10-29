7ac800f04e21800917231972c1cce29140b4c8f0
wangxingkang <wang_xingkang@qq.com>
2022-10-29 16:10:18 +0800

    feat: 去除首页跳转逻辑

--- modules/launcher/src/pages/home/index.tsx
+++ modules/launcher/src/pages/home/index.tsx
@@ -1,29 +1,22 @@
-import { proxy, useSnapshot } from 'valtio';
-import { useNavigate } from 'react-router-dom'
-import { Button } from 'antd';
+import { proxy, useSnapshot } from 'valtio'
+import { Button } from 'antd'
 
 const store = proxy({
   count: 0,
   actions: {
-	  add() {
+    add: () => {
 	    // 注意这里别用 this.count，基于 snap 调用时会报错
-	    store.count += 1;
-	  },
+      store.count += 1;
+    },
   }
 });
 
 export default function Home() {
-  const navigaten = useNavigate();
   const snap = useSnapshot(store);
-
-  const handleClock = () => {
-    snap.actions.add();
-    navigaten('/micro/video-monitor')
-  }
 
   return (
     <div style={{ height: '100vh' }}>
-      <Button onClick={handleClock}>点击{snap.count}</Button>
+      <Button onClick={snap.actions.add}>点击{snap.count}</Button>
     </div>
   )
 }
